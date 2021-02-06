4e7e3a036319f9f27bb3a9a1e9dd93b4e2e9d1e0
tidaaartorhem <aaditm2000@gmail.com>
2021-02-06 11:21:23 -0500

    Added Firebase db and initialized

--- app/App.js
+++ app/App.js
@@ -1,8 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase'
+import fire from 'fire'
+export default class App extends React.Component() {
 
-export default function App() {
+  constructor(props){
+    super(props);
+
+    //Initialize Firebase....
+    if(!firebase.apps.length){firebase.initializeApp(fire.FirebaseConfig);}
+
+}
+    ;
+render(){
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -10,12 +21,13 @@ export default function App() {
     </View>
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }, 
+}); 
+
