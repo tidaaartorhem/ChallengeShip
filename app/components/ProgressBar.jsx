dc8f5e49a5c31456e4f22679029cf5cdfd0cdf48
jliaz <juliazhang1108@gmail.com>
2021-02-06 10:43:55 -0500

    create componentt files

---
+++ app/components/ProgressBar.jsx
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+
+export default function ProgressBar (props) {
+    return (
+        <View></View>
+    )
+}
