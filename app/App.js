21bc6739f72cda6793e596eaacb15e382bc59b1f
jliaz <juliazhang1108@gmail.com>
2021-02-06 22:49:12 -0500

    screens

--- app/App.js
+++ app/App.js
@@ -2,17 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DailyPage from './screens/DailyPage';
+import Home from './screens/Home';
+import CreateGroup from './screens/CreateGroup';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-
-const 
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={DailyPage} />
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Create" component={CreateGroup} />
+        <Drawer.Screen name="Daily" component={DailyPage} />
       </Drawer.Navigator>
   </NavigationContainer>
   );
