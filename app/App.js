03b12d782f5841916fdacf73de69d46a00527bbd
Victor Sun <victorjsun@gmail.com>
2021-02-06 20:11:34 -0500

    Add weekly progress

--- app/App.js
+++ app/App.js
@@ -1,27 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as firebase from 'firebase'
-import fire from 'fire'
-export default class App extends React.Component() {
+import fire from './fire'
+import WeeklyTracker from './screens/WeeklyProgress'
 
-  constructor(props){
-    super(props);
-
-    //Initialize Firebase....
-    if(!firebase.apps.length){firebase.initializeApp(fire.FirebaseConfig);}
-
-}
-    ;
-render(){
+const App = () => {
+  useEffect(() => {
+    if(!firebase.apps.length){firebase.initializeApp(fire.FirebaseConfig)};
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WeeklyTracker></WeeklyTracker>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
-}
+export default App;
+
+// export default class App extends React.Component() {
+
+// //   constructor(props){
+// //     super(props);
+
+// //     //Initialize Firebase....
+// //     if(!firebase.apps.length){firebase.initializeApp(fire.FirebaseConfig);}
+
+// // }
+    
+// render(){
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+// }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ app/screens/WeeklyProgress.jsx
@@ -0,0 +1,90 @@
+
+import React, { Component, useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button
+} from 'react-native';
+import { Badge } from 'react-native-elements';
+import * as Progress from 'react-native-progress';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 40
+  },
+  progressBar: {
+    width: 100,
+  },
+  header: {
+    fontSize: 25,
+    margin: 40
+  },
+  progress: {
+    margin: 40
+  },
+  progressText: {
+    marginTop: 10,
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  progressIcons: {
+    margin: 40,
+    flexDirection: "row"
+  },
+  icon: {
+    margin: 10
+  },
+  returnButton: {
+    position:"absolute",
+    bottom: 50,
+  }
+});
+
+const days = [
+  {name: "S", progress: 70},
+  {name: "M", progress: 50},
+  {name: "T", progress: 70},
+  {name: "W", progress: 70},
+  {name: "T", progress: 30},
+  {name: "F", progress: 70},
+  {name: "S", progress: 70},
+]
+
+const toStatus = (progress) => {
+  if (progress >= 70) {
+    return "success"
+  } else if (progress >= 50) {
+    return "warning"
+  } else {
+    return "error"
+  }
+}
+
+export default function WeeklyTracker (props) {
+    const [progress, setProgress] = useState(0);
+    useEffect(() => {
+      setProgress(80)
+    }, [])
+    return (
+        <View style={styles.container}>
+          <Text style={styles.header}>Weekly Progress</Text>
+          <View style={styles.header}>
+            <Progress.Bar progress={progress/100} width={200} height={20}/>
+            <Text style={styles.progressText}>{progress}%</Text>
+          </View>
+          <View style={styles.progressIcons}>
+            {
+              days ? days.map((day, index) => (
+                <Badge key={index} width={25} badgeStyle={styles.icon} status={toStatus(day.progress)} value={<Text>{day.name}</Text>}></Badge>
+              )) : null
+            }
+          </View>
+          <Button style={styles.returnButton} color="black" title="Return" />
+        </View>
+    )
+}
